7dbd028deff8255f5cbc535f2c912923f7d3ab2e
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2014-10-25 12:31:52 +0100

    Initial commit of WebRTC/OpenTok server SDK with sample page at index.

--- TeamWin.Propinquity/TeamWin.Propinquity.Web/Bootstrapper.cs
+++ TeamWin.Propinquity/TeamWin.Propinquity.Web/Bootstrapper.cs
@@ -1,11 +1,16 @@
-﻿namespace TeamWin.Propinquity.Web
+﻿using HelloWorld;
+
+namespace TeamWin.Propinquity.Web
 {
 	using Nancy;
 
 	public class Bootstrapper : DefaultNancyBootstrapper
 	{
-		// The bootstrapper enables you to reconfigure the composition of the framework,
-		// by overriding the various methods and properties.
-		// For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+        protected override void ConfigureApplicationContainer(Nancy.TinyIoc.TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            container.Register<OpenTokService>().AsSingleton();
+        }
 	}
 }
--- TeamWin.Propinquity/TeamWin.Propinquity.Web/IndexModule.cs
+++ TeamWin.Propinquity/TeamWin.Propinquity.Web/IndexModule.cs
@@ -1,15 +1,24 @@
-﻿namespace TeamWin.Propinquity.Web
+﻿using System.Dynamic;
+using HelloWorld;
+
+namespace TeamWin.Propinquity.Web
 {
 	using Nancy;
 
 	public class IndexModule : NancyModule
 	{
-		public IndexModule()
+        public IndexModule(OpenTokService opentokService)
 		{
-			Get["/"] = parameters =>
-			{
-				return View["index"];
-			};
+            Get["/"] = _ =>
+            {
+                dynamic locals = new ExpandoObject();
+
+                locals.ApiKey = opentokService.OpenTok.ApiKey.ToString();
+                locals.SessionId = opentokService.Session.Id;
+                locals.Token = opentokService.Session.GenerateToken();
+
+                return View["index", locals];
+            };
 		}
 	}
 }
