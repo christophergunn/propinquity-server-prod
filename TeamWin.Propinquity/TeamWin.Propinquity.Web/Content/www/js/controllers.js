6983897cff1d3129b3b4e6c182ae77bf82fe6ead
Anthony Fielding <anthony.fielding@drllimited.co.uk>
2014-10-25 21:05:02 +0100

    Chat window background, re-enable video

--- TeamWin.Propinquity/TeamWin.Propinquity.Web/Content/www/js/controllers.js
+++ TeamWin.Propinquity/TeamWin.Propinquity.Web/Content/www/js/controllers.js
@@ -9,20 +9,19 @@ angular.module('starter.controllers', [])
 	var session = TB.initSession(sessionId);
 	var uuid = getUuid();
 
-	// Initialize a Publisher, and place it into the element with id="publisher"
-	var publisher = TB.initPublisher(apiKey, 'publisher');
-
 	$scope.messages = []
+
+	var publisher = TB.initPublisher(apiKey, 'publisher');
 
 	// Attach event handlers
 	session.on({
-	    // This function runs when session.connect() asynchronously completes
-	    sessionConnected: function (event) {
+
+		sessionConnected: function (event) {
 	        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
 	        // clients)
 	        session.publish(publisher);
 	    },
-
+	    
 	    // This function runs when another client publishes a stream (eg. session.publish())
 	    streamCreated: function (event) {
 	        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
@@ -93,19 +92,19 @@ angular.module('starter.controllers', [])
 	    sessionConnected: function (event) {
 	        // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
 	        // clients)
-	        //session.publish(publisher);
+	        session.publish(publisher);
 	    },
 
 	    // This function runs when another client publishes a stream (eg. session.publish())
 	    streamCreated: function (event) {
-	        // // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
-	        // // the element with id="subscribers"
-	        // var subContainer = document.createElement('div');
-	        // subContainer.id = 'stream-' + event.stream.streamId;
-	        // document.getElementById('subscribers').appendChild(subContainer);
+	        // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
+	        // the element with id="subscribers"
+	        var subContainer = document.createElement('div');
+	        subContainer.id = 'stream-' + event.stream.streamId;
+	        document.getElementById('subscribers').appendChild(subContainer);
 
-	        // // Subscribe to the stream that caused this event, put it inside the container we just made
-	        // session.subscribe(event.stream, subContainer);
+	        // Subscribe to the stream that caused this event, put it inside the container we just made
+	        session.subscribe(event.stream, subContainer);
 	    }
 	});
 
