5d74581251e9afad52941276b4ec8449b6ae3322
Christopher Gunn <christopher.gunn@drllimited.co.uk>
2014-10-25 21:57:27 +0100

    Add comprehensive logging.

--- TeamWin.Propinquity/TeamWin.Propinquity.Web/Content/js/index.js
+++ TeamWin.Propinquity/TeamWin.Propinquity.Web/Content/js/index.js
@@ -1,32 +1,48 @@
-﻿var session;
+﻿var sessionId;
+var session;
 var publisher;
 
 function onSessionIdChanged(sessionId, token) {
     if (session) {
-        session.unpublish(publisher);
+        console.log('onSessionIdChanged, disconnecting the previous session...');
+        session.disconnect();
+        session.on({
+            sessionDisconnected: function(event) {
+                console.log("The session disconnected: " + event.reason);
+                initializeSession(sessionId, token);
+            }
+        });
+    } else {
+        console.log('onSessionIdChanged, no previous session, initializing straight away...');
+        initializeSession(sessionId, token);
     }
+}
 
-	// Initialize an OpenTok Session object
+function initializeSession(sessionId, token) {
+    // Initialize an OpenTok Session object
     session = TB.initSession(sessionId);
-    
+
     // Initialize a Publisher, and place it into the element with id="publisher"
     publisher = TB.initPublisher(apiKey, 'publisher');
 
     // Attach event handlers
     session.on({
         // This function runs when session.connect() asynchronously completes
-        sessionConnected: function(event) {
+        sessionConnected: function (event) {
+            console.log("sessionConnected: " + event.target.sessionId);
             // Publish the publisher we initialzed earlier (this will trigger 'streamCreated' on other
             // clients)
             session.publish(publisher);
         },
-        
-        sessionDisconnected: function(event) {
-        	console.log("The session disconnected: " + event.reason);
+
+        sessionDisconnected: function (event) {
+            console.log("sessionDisconnected: " + event.reason);
         },
 
         // This function runs when another client publishes a stream (eg. session.publish())
-        streamCreated: function(event) {
+        streamCreated: function (event) {
+            console.log('streamCreated: event.stream.streamId: ' + event.stream.streamId);
+
             // Create a container for a new Subscriber, assign it an id using the streamId, put it inside
             // the element with id="subscribers"
             var subContainer = document.createElement('div');
@@ -36,30 +52,32 @@ function onSessionIdChanged(sessionId, token) {
             // Subscribe to the stream that caused this event, put it inside the container we just made
             session.subscribe(event.stream, subContainer);
         },
-        
-        streamDestroyed: function(event) {
-            console.log("Stream " + event.stream.name + " ended. " + event.reason);
+
+        streamDestroyed: function (event) {
+            console.log("streamDestroyed: the name is " + event.stream.name + ", the reason is " + event.reason + '.');
         },
-        
-        connectionDestroyed: function(event) {
-            console.log('A client disconnected. Id: ' + event.connection.connectionId);
+
+        connectionDestroyed: function (event) {
+            console.log('connectionDestroyed: connection Id: ' + event.connection.connectionId);
         }
     });
-    
+
     // Connect to the Session using the 'apiKey' of the application and a 'token' for permission
     session.connect(apiKey, token);
 }
 
-var sessionId;
-
 $(function () {
     var uuid = getUuid();
-    geoFindMe(function(lat, lon) {
+    console.log('Document ready, uuid is: ' + uuid + '.');
+    geoFindMe(function (lat, lon) {
+        console.log('Got GPS, lat: ' + lat + ', lon: ' + lon + '.');
+        
         $.post(serverUrl + "client/gps", { id: uuid, lat: lat, lon: lon })
          .done(function (data) {
-         	if (sessionId != data.SessionId) {
-         		sessionId = data.SessionId;
-		        onSessionIdChanged(data.SessionId, data.Token);
+             if (sessionId != data.SessionId) {
+                 console.log('Posted GPS and session changed from: ' + sessionId + ', to: ' + data.SessionId + '.');
+         		 sessionId = data.SessionId;
+		         onSessionIdChanged(data.SessionId, data.Token);
 	         }
          });
     });
