0876055090a5da9ed815acd94dbda7deaa0ede32
unknown <phillc@GBBLL5617.drl.local>
2014-10-25 12:15:36 +0100

    Initial nancy web project

---
+++ TeamWin.Propinquity/TeamWin.Propinquity.Web/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace TeamWin.Propinquity.Web
+{
+	using Nancy;
+
+	public class IndexModule : NancyModule
+	{
+		public IndexModule()
+		{
+			Get["/"] = parameters =>
+			{
+				return View["index"];
+			};
+		}
+	}
+}
